e82757aca3242ae93c065c36dc4ed4579a167850
Work <Work@MSI>
2024-04-11 10:27:53 -0500

    Where we ended on April 11

--- 1436-Chapter7/April9.cpp
+++ 1436-Chapter7/April9.cpp
@@ -1,41 +1,92 @@
 #include <iostream>
 
+#include<string>
+
 using namespace std;
 
 char at(string inputString, int index)
 {
-    return inputString.at(0); 
+    return inputString.at(index); 
 }
 
 //demo function overloading!
+void printSum(int a, int b)
+{
+    cout << a << " + " << b << " = " << a + b << endl; 
+}
+
+/*this overload takes three string and returns nuthin'!*/
+void printSum(string a, string b, string c)
+{
+    cout << a + b + c << endl; 
+}
+
+/*THIS overload returns a STRING and takes two characters*/
+string printSum(char a, char b)
+{
+    string first = std::to_string(a); 
+    string second = std::to_string(b); 
+
+    string result = first + second; 
+
+    return result;
+
+}
+
 
 int main()
 {
-    //string a = "0123456"; //zeroth, first, second, third 
-    
-    //cout << at(a, 0) << endl; //what does THIS print? 
-    //cout << a.at(6) << endl; 
-    //cout << "The number of chars in the string " << a << " is: " << a.size() << endl; 
-        //size does NOT (necessarily) return the number of bytes
-    
-    string greeting = "Hllo, e";
+    ////string a = "0123456"; //zeroth, first, second, third 
+    //
+    ////cout << at(a, 0) << endl; //what does THIS print? 
+    ////cout << a.at(6) << endl; 
+    ////cout << "The number of chars in the string " << a << " is: " << a.size() << endl; 
+    //    //size does NOT (necessarily) return the number of bytes
+    //
+    //string greeting = "Hllo, e";
 
-    string name = "Seth";
-    
-    // the '+' operator can be used to join two (or more) strings 
-    string fullGreeting = greeting + name;
+    //string name = "Seth";
+    //
+    //// the '+' operator can be used to join two (or more) strings 
+    ////+ means "concatenation" when it is applied to "string" types 
+    //string fullGreeting = greeting + name + "asdfasdfasdf";
   
-    cout << fullGreeting << endl;
-    //"subscript" (or index) operator is (square) brackets 
-    //{}
-    //[]
-    //    ()
-    //an index is a thing in the back of a textbook 
+    //cout << fullGreeting << endl;
+    ////"subscript" (or index) operator is (square) brackets 
+    ////{}
+    ////[]
+    ////    ()
+    ////an index is a thing in the back of a textbook 
 
-    cout << fullGreeting[1] << endl; 
-    //Hello, Seth 
-    cout << "the find function found the character 'e' at index = " << 
-        fullGreeting.find('e') << endl;
+    ////"1234abc"
+    //string otherString = "1234abc";
+
+    //cout <<"The thing I just typed yields: " << otherString[4] << endl;
+    //cout << fullGreeting[1] << endl; 
+    ////Hello, Seth 
+    //cout << "the find function found the character 'e' at index = " << 
+    //    fullGreeting.find('e') << endl;
+
+    //size_t
+    string otherExample = "alphabet";
+    int firstPositionAtWhichAIsFound = otherExample.find('a'); 
+    cout << otherExample << " has an 'a' at index: " << firstPositionAtWhichAIsFound<< endl;
+
+
+    cout << otherExample << " has ANOTHER 'a' at index: "
+        << otherExample.find('a', firstPositionAtWhichAIsFound + 1)  << endl;
+    //printSum()
+    //printSum(1, 2);
+    //printSum("I do", "not like", "green eggs and ham");
+
+    cout << "What is length of " << otherExample << " ... it is: " << otherExample.length() << endl; 
+
+    for (int i = 0; i < otherExample.length(); i++)
+    {
+        cout << (char)(toupper(otherExample.at(i))) << "\t"; //tab 
+    }
+
+
 
 
     return 0;
---
+++ aPRIL11/April11.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+void findCharacterInString(string inputString, char targetChar)
+{
+    int numberOfOccurences = 0; //optional
+    for (int i = 0; i < inputString.length(); i++)
+    {
+        if (inputString[i] == targetChar)
+        {
+            cout << "Found at index " << i << endl;
+            numberOfOccurences++;
+        }
+    }
+    cout << "Found '" << targetChar << "' "
+        << numberOfOccurences << " times." << endl;
+}
+
+void myReplace(string input, char charToReplace)
+{
+    //...insert function definition (figure it out on your own, if you like) 
+}
+int main()
+{
+
+    string someString = "2345678";
+    cout << "How to replace a SINGLE character?\n";
+    //someString.replace(3, 2, "a"); //234a78?
+
+   cout << someString.substr(3, 2) << endl; //substring is similar to math word: subset
+
+    //cout << someString << endl; 
+
+    //while (true) //intentional infinite loop 
+    //{
+    //    string goofyCityName = "Kalamazoo";
+    //    cout << "Enter the character you want to look for ...\n";
+    //    char characterToSearchFor;
+
+    //    cin >> characterToSearchFor;
+
+    //    findCharacterInString(goofyCityName, characterToSearchFor);
+    //}
+
+
+
+    //string aSoundADogMakes = "woof!"; 
+    //cout << "Initial sound that a dog makes (upon approaching a fence): " << aSoundADogMakes << endl; 
+    //aSoundADogMakes.append("WOOF BARK!");
+
+    //cout << "After exacerbating the situation by coming closer and poking a stick through the fence: "
+    //    << aSoundADogMakes << endl; 
+    //
+    //NOTE that appending is different from concatenation (+ operator on strings) 
+
+    //string emptyString = "adsfasdfsadm";
+    //if (emptyString.empty() == true)
+    //{
+    //    cout << "That string is empty (ie: it has 0 characters inside of it)\n"; 
+    //}
+    //else
+    //{
+    //    cout << emptyString << " is NOT empty (it DOES have some characters)\n";
+
+    //    cout << "Let's make it empty for you ...\n";
+
+    //    emptyString.clear(); 
+
+    //    //\n makes a blank line (more space to read the text) 
+    //    cout << "\n\nNOW that string is: " << emptyString << endl; 
+    //}
+
+    return 0;
+}
